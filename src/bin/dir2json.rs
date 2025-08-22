db0106dbf2078b53f80d37e4b61bc2497a6cb023
Alan Urmancheev <alan.urman@gmail.com>
2025-08-22 17:25:37 +0400

    Add a stub for dir2json

---
+++ src/bin/dir2json.rs
@@ -0,0 +1,11 @@
+/*
+TODO:
+
+- Create a module for handling command line arguments.
+- Create a module for reading the FS.
+- Create a module for converting that to JSON.
+*/
+
+use std::fs;
+
+fn main() {}
