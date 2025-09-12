d6fda5d8612d4727c5c51441eeaf43f18aedaf98
Alan Urmancheev <alan.urman@gmail.com>
2025-09-12 17:03:01 +0400

    Stubify dir2json again

--- src/bin/dir2json/main.rs
+++ src/bin/dir2json/main.rs
@@ -26,21 +26,63 @@ Main problems: tab completion, reliance on brace expansion, verbosity, completen
 dir2json foo bar { quux \\{ foo \\} }
 
 Execline can do the escaping, actually.
+
+# Should json2dir have a syntax to extract only a subset of the archive?
+
+dir2json # Nothing.
+dir2json foo # Only the pathname foo.
+dir2json foo/ # The pathname foo and all of its children.
+dir2json . # Nothing.
+dir2json ./ # Everything.
+dir2json ./{foo,bar,baz}
+
+dir2json # Nothing.
+dir2json foo
+dir2json {foo,bar,baz}
+
+dir2json src flake.nix
+
+dir2json src/
+
+dir2json src/json2dir/main.rs
+
+foo -- represents the path foo only
+dir2json . is allowed
+
+dir2json --help
+Warning: --help is interpreted as a pathname.
+
+dir2json .
+dir2json foo bar
+dir2json foo/bar
+
+type Subset = HashMap<Vec<u8>, Pathspec>;
+type Pathspec = Option<Subset>;
 */
 
 // dir2json . foo main.go **/*.go
 
-use std::collections::HashMap;
+// use std::collections::HashMap;
+// use std::fs;
 
-type Dir = HashMap<Vec<u8>, File>;
+// type Dir = HashMap<Vec<u8>, File>;
 
-enum File {
-    Dir(Dir),
-    Regular,
+// enum File {
+//     Dir(Dir),
+//     Regular,
+// }
+
+// fn parse() -> Dir {
+//     todo!()
+// }
+
+// type Pathspec = Option<Entries>;
+// struct Entries(HashMap<Vec<u8>, Pathspec>);
+
+// fn _dir2json(_pathspec: Pathspec) {
+
+// }
+
+fn main() {
+    // fs::File::open(".").unwrap();
 }
-
-fn parse() -> Dir {
-    todo!()
-}
-
-fn main() {}
